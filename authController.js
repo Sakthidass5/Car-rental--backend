63aa9dc5d781f1e16e782a1fb59255449f3dbbcb
Sakthidass5 <146849314+Sakthidass5@users.noreply.github.com>
2025-10-22 11:04:57 +0530

    Update authController.js

--- authController.js
+++ authController.js
@@ -10,11 +10,19 @@ exports.register = async (req, res) => {
     if (existingUser) {
       return res.status(409).json({ error: 'Email already registered' });
     }
+console.log(role)
+    if (role === 'admin') {
+      const existingAdmin = await User.findOne({ role: 'admin' });
+      console.log(existingAdmin,'existingAdmin')
+      if (existingAdmin) {
+        return res.status(403).json({ error: 'Admin already registered' });
+      }
+    }
 
     const user = new User({ name, email, password, role });
     await user.save();
 
-    res.status(201).json({ message: 'User registered' });
+    res.status(201).json({status:true, message: 'User registered successfully' });
   } catch (err) {
     console.error('Registration error:', err.message);
     res.status(500).json({ error: 'Server error during registration' });
@@ -25,13 +33,29 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log(req.body, 'req.body');
+
     const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Email not registered' });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Incorrect password' });
+    }
+
     const token = generateToken(user);
-    res.json({ token, role: user.role });
+    console.log(token, 'token');
+
+ res.status(200).json({
+      message: 'Login successful',
+      token,
+      role: user.role,
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
